db7e4d14c04dc86e709af813d895bf874d71900b
FELIX RAFAEL MARTINEZ MACHUCA <I21050342@monclova.tecnm.mx>
2021-10-19 14:58:16 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleArreglo 19.10.21/Alumno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_19._10._21
+{
+    class Alumno
+    {
+        protected string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        protected string matricula;
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        protected string carrera;
+
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+        public Alumno()
+        {
+            nombre = "";
+            matricula = "";
+            carrera = "";
+        }
+
+        public Alumno(string matricula, string carrera)
+        {
+            this.Nombre = nombre;
+            this.matricula = matricula;
+            this.carrera = carrera;
+        }
+
+        public override string ToString()
+        {
+            return "nombre " + nombre + "\nmatricula " + matricula + "\ncarrera " + carrera;
+        }
+    }
+}
---
+++ ConsoleArreglo 19.10.21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Console_19._10._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno[] a = new Alumno[3];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = new Alumno();
+                Console.WriteLine("Nombre del alumno");
+                a[i].Nombre = Console.ReadLine();
+                Console.WriteLine("Numero de Matricula");
+                a[i].Matricula = Console.ReadLine();
+                Console.WriteLine("Nombre de la carrera");
+                a[i].Carrera = Console.ReadLine();
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i].ToString());
+            }
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
